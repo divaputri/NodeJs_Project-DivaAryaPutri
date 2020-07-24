1a6e5286e0ca5f33eb25da414582a98e66a2c4f2
divaputri <68266016+divaputri@users.noreply.github.com>
2020-07-24 11:55:02 +0700

    index.js

--- thedir/api/index.js
+++ thedir/api/index.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const express = require('express');
+const { json } = require('express');
 const app = express();
 
 app.use(express.json());
@@ -34,7 +35,7 @@ app.get('/api/users/:username/:password', (req, res) => {
     console.log(req.params);
 
     // validate
-    /* const {error} = validateUser(req.params);
+    const {error} = validateUser(req.params);
     if (error) {
         console.log('Validation error');
         
@@ -44,7 +45,7 @@ app.get('/api/users/:username/:password', (req, res) => {
         }
         return res.status(400).json(jsonRespond)
     }
-    console.log('Validation success'); */
+    console.log('Validation success');
 
     //check if username and password coreect
     console.log('Check existing username : ' + req.params.username + ' and password : ' + req.params.password);
@@ -109,9 +110,51 @@ app.post('/api/users', (req, res) => {
         password: req.body.password
     };
 
-    user.push(user);
+    users.push(user);
     return res.json(user); 
 });
+
+
+const listing = [
+    {id: 1, name: 'Blooming Buds Bali', city: 'Denpasar', phone: '0812347321', address: 'Gunung Agung 48' , category: 'Shopping'},
+    {id: 2, name: 'Brownfox Waffle and Coffee', city: 'Denpasar', phone: '081802392', address: 'Raya Puputan No. 80', category: 'Food and Restaurant'},
+    {id: 3, name: 'Ayam Betutu khas Gilimanuk', city: 'Denpasar', phone: '03612183', address: 'Merdeka No.88', category: 'Food and Restaurant'},
+    {id: 4, name: 'Massimo Italian Restaurant', city: 'Denpasar', phone: '03612394', address: 'Danau Tamblingan No.228', category: 'Food and Restaurant'}
+];
+
+// LIST ALL directory
+app.get("/api/listing", (req, res) => {
+    //res.setHeader('Access-Control-Allow-Origin', '*');
+    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    var datetime = new Date();
+    console.log("\n" + datetime);
+    console.log('Listing success');
+    return res.json(listing);
+
+    //res.send(listing);
+});
+
+app.get("/api/listing/:id", (req, res) => {
+
+    var datetime = new Date();
+    console.log("\n" + datetime);
+    console.log("Incoming new GET HTTP request");
+    console.log(req.body);
+
+    const list = listing.find( l => l.id === parseInt(req.params.id));
+    if ( !list ) res.status(404).send('ID not found.');
+
+    res.send(listing);
+    
+    /*console.log('List found');
+    var jsonRespond = {
+        return: list,
+        message: "List found"
+    }
+    return res.json(jsonRespond); */
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
@@ -121,7 +164,7 @@ app.listen(port, () => {
 function validateUser(user){
     const schema = Joi.object({
         username: Joi.string().min(3),
-        email: Joi.string().email({ minDomainAtoms: 2, tlds: { allow: ['com', 'net']}}),
+        email: Joi.string().email({ minDomainSegments : 2, tlds: { allow: ['com', 'net']}}),
         password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
     });
     return schema.validate(user);
